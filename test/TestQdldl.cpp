3220bdf4591f7a6af49abf0633673b43041d8d72
Prajish Sankar <psankar@anybotics.com>
2020-08-28 17:11:30 +0000

    Merge branch 'fix/add_test_cov_prajish_list' into 'master'
    [motion_control] Add test coverage for some packages
    
    GitOrigin-RevId: 4c861d3e9954cfd4eec98d1c88278277f241a0e6

---
+++ test/TestQdldl.cpp
@@ -0,0 +1,12 @@
+/*!
+ * @file        TestQdldl.cpp
+ * @authors     Prajish Sankar (ANYbotics)
+ * @brief       Tests for the qdldl package.
+ * @note        Contains just a dummy test.
+ */
+
+#include <gtest/gtest.h>
+
+TEST(TestQdldl, dummyTest) {  // NOLINT
+  ASSERT_TRUE(true);
+}
